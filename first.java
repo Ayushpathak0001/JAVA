721572491b48022e9bed9215e344555b7317db29
Ayushpathak0001 <ayushraj132007@gmail.com>
2026-06-29 11:36:28 +0530

    javastarting

---
+++ first.java
@@ -0,0 +1,11 @@
+class Firstclass{
+    public static void main(String[] args){
+    System.out.println("hello,world");
+}
+}
+
+class student{
+    public static void main(String[]args){
+        System.out.println("Ayush");
+    }
+}
---
+++ star.java
@@ -0,0 +1,8 @@
+class star{
+    public static void main(String[]args){
+        int a=25, b=42;
+        System.out.println(2*(a+b));
+       
+    }
+
+}
