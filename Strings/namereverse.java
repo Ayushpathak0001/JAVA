21bad5a1a9a2804b91ac2a032cd0f52b73270600
Ayushpathak0001 <ayushraj132007@gmail.com>
2026-07-06 18:59:20 +0530

    reverseString

---
+++ Strings/namereverse.java
@@ -0,0 +1,11 @@
+
+
+public class namereverse {
+    public static void main(String[]args){
+        String sb="Tony stark";
+        for(int i=sb.length()-1;i>=0;i--){
+            System.out.println(sb.charAt(i));
+        }
+    }
+    
+}
