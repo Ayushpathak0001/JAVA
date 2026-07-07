ba332b719e0dcb8be00e739ca94b43628dbf6d83
Ayushpathak0001 <ayushraj132007@gmail.com>
2026-07-08 00:50:51 +0530

    changingBits

---
+++ bitmanipulation/setbit/pos1.java
@@ -0,0 +1,11 @@
+
+
+public class pos1 {
+    public static void main(String[]args){
+        int num=5;
+        int pos=1;
+        int bitMask=1<<pos;
+        int newNumber=bitMask | num;
+        System.out.println(newNumber);
+    }
+}
