6867c16f23a2d086a0ffeea9cf1afc6fbc477e10
Ayushpathak0001 <ayushraj132007@gmail.com>
2026-07-10 22:21:45 +0530

    bitmanipulation

--- bitmanipulation/clearbit/pos2.java
+++ bitmanipulation/clearbit/pos2.java
@@ -4,7 +4,7 @@ public class pos2 {
     public static void main(String[]args){
         int num=5;
         int pos=2;
-        int bitMask=(1<<2);
+        int bitMask=(1<<pos);
         int notBitMask=~(bitMask);
         int newNumber=notBitMask & num;
         System.out.println(newNumber);
