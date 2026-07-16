22a1e882c7a5edcfa2b1a40f2bc17c07293a43cf
Ayushpathak0001 <ayushraj132007@gmail.com>
2026-07-17 02:53:49 +0530

    check if the number is in correct order

---
+++ DSA IN JAVA/Recursion/isnoSorted.java
@@ -0,0 +1,19 @@
+public class isnoSorted {
+
+    public static boolean isSorted(int arr[],int idx){
+        if(idx==arr.length-1){
+            return true;
+        }
+        if(arr[idx]<arr[idx+1]){
+            return isSorted(arr,idx+1);
+        }else{
+            return false;
+        }
+    }
+    public static void main(String[]args){
+        int arr[]={1,2,3};
+        System.out.print(isSorted(arr,0));
+    }
+}
+
+// time complexity O(n)
