07659abce285cdc82385f0df89ac4703ab826e28
Ayushpathak0001 <ayushraj132007@gmail.com>
2026-07-20 15:33:51 +0530

    Basic Function

---
+++ LinkedLIst/bscFunc.java
@@ -0,0 +1,64 @@
+
+
+public class bscFunc {
+     Node head;
+    class Node{
+        String data;
+        Node next;
+       
+
+        Node(String data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+
+    //add -first
+    public void addFirst(String data){
+        Node newNode= new Node(data);
+        if(head==null){
+            head=newNode;
+            return;
+        }
+        newNode.next=head;
+        head=newNode;
+    } 
+    //add last
+    public void addLast(String data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=newNode;
+            return;
+        }
+        Node currNode=head;
+        while(currNode.next!=null){
+            currNode=currNode.next;
+        }
+    }
+    //print
+    public void printList(){
+        if(head==null){
+            System.out.println("List is empty");
+            return;
+        }
+        Node currNode=head;
+        while(currNode!=null){
+            System.out.print(currNode.data+" -> ");
+            currNode=currNode.next;
+        }
+        System.out.println("Null");
+    }
+
+
+    
+    public static void main(String[]args){
+        bscFunc list=new bscFunc();
+        list.addFirst("a");
+        list.addFirst("b");
+        list.addFirst("c");
+        list.addLast("d");
+        list.addLast("f");
+        list.printList();
+      
+    }
+}
